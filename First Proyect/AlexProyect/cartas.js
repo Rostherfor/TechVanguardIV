203ae918e049cf3f99a14ab2c5ee879473224a93
MikeHB <128311830+Rostherfor@users.noreply.github.com>
2023-04-06 20:53:27 -0600

    Add files via upload

---
+++ First Proyect/AlexProyect/cartas.js
@@ -0,0 +1,11 @@
+
+setTimeout(() => {
+document.querySelector(".carta3").style.gridArea="1/1/3" 
+document.querySelector(".carta3").style.transition="5s" 
+}, 3900);
+
+setTimeout(() => {
+    document.querySelector(".carta4").style.gridArea="2/2" 
+    document.querySelector(".carta3").style.transition="5s" 
+    }, 3000);
+    
