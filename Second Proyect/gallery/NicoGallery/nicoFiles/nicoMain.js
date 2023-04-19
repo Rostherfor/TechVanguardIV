6d62ad7e9847f5515ff623b7b3ed91b9a53919e6
NightShift0666 <niko87354mc@gmail.com>
2023-04-19 13:20:36 -0500

    Carrusel automatizado

--- Second Proyect/gallery/NicoGallery/nicoFiles/nicoMain.js
+++ Second Proyect/gallery/NicoGallery/nicoFiles/nicoMain.js
@@ -10,109 +10,175 @@ const btnRight = document.querySelector(".carrusel-local__btn-right");
 const btnLeft = document.querySelector(".carrusel-local__btn-left");
 
 carruselCards.insertAdjacentElement('afterbegin', carruselCardLast);
+let funcionSliderBig;
+let funcionSlider;
+
+if(window.innerWidth >= 769){
+    funcionSliderBig = setInterval(sliderBig, 3000);
+}
+
+if(window.innerWidth <= 768){
+
+    funcionSlider = setInterval(slider, 3000);
+}
 
 
+function sliderBig(){
 
-    
+
     
         
-        function next() {
-
-
+            
             let carruselCardFirst = document.querySelectorAll(".carrusel-local__card")[0];
-            carruselCards.style.marginLeft = "-200%";
+            carruselCards.style.marginLeft = "-66%";
             carruselCards.style.transition = "all .5s ease";
             setTimeout(function () {
-
+    
                 carruselCards.style.transition = 'none';
                 carruselCards.insertAdjacentElement('beforeend', carruselCardFirst);
-                carruselCards.style.marginLeft = '-100%'
-
+                carruselCards.style.marginLeft = '-33%'
+    
             }, 500)
-        }
-
-
-        btnRight.addEventListener('click', () => {
-            next()
-        });
-
-
-        function preview() {
-
-
-            let carruselCard = document.querySelectorAll(".carrusel-local__card");
-            let carruselCardLast = carruselCard[carruselCard.length - 1];
-            carruselCards.style.marginLeft = "0%";
-            carruselCards.style.transition = "all .5s ease";
-            setTimeout(function () {
-
-                carruselCards.style.transition = 'none';
-                carruselCards.insertAdjacentElement('afterbegin', carruselCardLast);
-                carruselCards.style.marginLeft = '-100%'
-
-            }, 500)
-        }
-
-        btnLeft.addEventListener('click', () => {
-            preview()
-        });
-    
-    
-
-        if(window.innerWidth > 768){
-
-            function nextBig() {
-
-
-                let carruselCardFirst = document.querySelectorAll(".carrusel-local__card")[0];
-                carruselCards.style.marginLeft = "-66%";
-                carruselCards.style.transition = "all .5s ease";
-                setTimeout(function () {
-   
-                    carruselCards.style.transition = 'none';
-                    carruselCards.insertAdjacentElement('beforeend', carruselCardFirst);
-                    carruselCards.style.marginLeft = '-33%'
-   
-                }, 500)
-            }
-   
-   
-            btnRight.addEventListener('click', () => {
-                nextBig();
-               
-            });
-   
-   
-            function previewBig() {
-   
-   
-                let carruselCard = document.querySelectorAll(".carrusel-local__card");
-                let carruselCardLast = carruselCard[carruselCard.length - 1];
-                carruselCards.style.marginLeft = "0%";
-                carruselCards.style.transition = "all .5s ease";
-                setTimeout(function () {
-   
-                    carruselCards.style.transition = 'none';
-                    carruselCards.insertAdjacentElement('afterbegin', carruselCardLast);
-                    carruselCards.style.marginLeft = '-33%'
-   
-                }, 500)
-            }
-   
-   
-            btnLeft.addEventListener('click', () => {
-                previewBig();
-               
-            });
-        }
-    
-         
-    
-
         
 
 
+}
+
+function slider(){
+
 
     
+        
+            
+    let carruselCardFirst = document.querySelectorAll(".carrusel-local__card")[0];
+    carruselCards.style.marginLeft = "-200%";
+    carruselCards.style.transition = "all .5s ease";
+    setTimeout(function () {
+
+        carruselCards.style.transition = 'none';
+        carruselCards.insertAdjacentElement('beforeend', carruselCardFirst);
+        carruselCards.style.marginLeft = '-100%'
+
+    }, 500)
+
+
+
+}
+
+    
+
+
+
+function next() {
+
+
+    let carruselCardFirst = document.querySelectorAll(".carrusel-local__card")[0];
+    carruselCards.style.marginLeft = "-200%";
+    carruselCards.style.transition = "all .5s ease";
+    setTimeout(function () {
+
+        carruselCards.style.transition = 'none';
+        carruselCards.insertAdjacentElement('beforeend', carruselCardFirst);
+        carruselCards.style.marginLeft = '-100%'
+
+    }, 500)
+}
+
+
+btnRight.addEventListener('click', () => {
+    next()
+    clearInterval(funcionSlider);
+});
+
+
+function preview() {
+
+
+    
+    let carruselCard = document.querySelectorAll(".carrusel-local__card");
+    let carruselCardLast = carruselCard[carruselCard.length - 1];
+    carruselCards.style.marginLeft = "0%";
+    carruselCards.style.transition = "all .5s ease";
+    setTimeout(function () {
+
+        carruselCards.style.transition = 'none';
+        carruselCards.insertAdjacentElement('afterbegin', carruselCardLast);
+        carruselCards.style.marginLeft = '-100%'
+
+    }, 500)
+}
+
+btnLeft.addEventListener('click', () => {
+    preview()
+    clearInterval(funcionSlider);
+});
+
+
+
+if (window.innerWidth > 768) {
+
+    function nextBig() {
+
+
+        
+        let carruselCardFirst = document.querySelectorAll(".carrusel-local__card")[0];
+        carruselCards.style.marginLeft = "-66%";
+        carruselCards.style.transition = "all .5s ease";
+        setTimeout(function () {
+
+            carruselCards.style.transition = 'none';
+            carruselCards.insertAdjacentElement('beforeend', carruselCardFirst);
+            carruselCards.style.marginLeft = '-33%'
+
+        }, 500)
+    }
+
+
+    btnRight.addEventListener('click', () => {
+        nextBig();
+        clearInterval(funcionSliderBig);
+        
+
+    });
+
+
+    function previewBig() {
+
+
+        
+        let carruselCard = document.querySelectorAll(".carrusel-local__card");
+        let carruselCardLast = carruselCard[carruselCard.length - 1];
+        carruselCards.style.marginLeft = "0%";
+        carruselCards.style.transition = "all .5s ease";
+        setTimeout(function () {
+
+            carruselCards.style.transition = 'none';
+            carruselCards.insertAdjacentElement('afterbegin', carruselCardLast);
+            carruselCards.style.marginLeft = '-33%'
+
+        }, 500)
+    }
+
+
+    btnLeft.addEventListener('click', () => {
+        previewBig();
+        clearInterval(funcionSliderBig);
+        
+        
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
