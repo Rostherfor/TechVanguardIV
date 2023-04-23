8dd58be4c8046a9bb7d3ff587e60a7c752b5816d
ResAgado0 <jesus.damaro35@gmail.com>
2023-04-23 02:00:12 -0500

    Actualización del footer

--- Second Proyect/gallery/JesusGallery/code/jesus.js
+++ Second Proyect/gallery/JesusGallery/code/jesus.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 0, lng: 0 },
     zoom: 8,
     });
 }
